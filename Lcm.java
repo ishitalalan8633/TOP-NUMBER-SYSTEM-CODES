1ded426dbe1caf49cff526d2b66b347e015f9a70
ishitalalan8633 <134314918+ishitalalan8633@users.noreply.github.com>
2024-11-19 12:41:04 +0530

    Create Lcm.java

---
+++ Lcm.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	    try{
+	        int a = Integer.parseInt(bf.readLine());
+	        int b = Integer.parseInt(bf.readLine());
+	        int gcd = gcdNo(a,b);
+	        int lcm =(a*b)/gcd;
+	        System.out.println(lcm);
+	    }
+	    catch(IOException e){
+	        System.out.println("An error occured while input reading");
+	    }
+	    catch(NumberFormatException e){
+	        System.out.println("Invalid number");
+	    }
+	}
+	static int gcdNo(int a, int b){
+	    while(a>0 && b>0){
+	        if(a>b){
+	            a = a%b;
+	        }
+	        else{
+	            b = b%a;
+	        }
+	    }
+	    if(a == 0){
+	        return b;
+	    }
+	    return a;
+	}
+}
