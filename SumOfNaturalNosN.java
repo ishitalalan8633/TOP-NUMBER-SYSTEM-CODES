84090e9391adf0658d82165b4b8795c07a7599f9
ishitalalan8633 <134314918+ishitalalan8633@users.noreply.github.com>
2024-11-19 16:11:41 +0530

    Create SumOfNaturalNosN.java

---
+++ SumOfNaturalNosN.java
@@ -0,0 +1,24 @@
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+	  BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	  try{
+	      int a = Integer.parseInt(bf.readLine());
+	      int s = sumOfNaturalNos(a);
+	      System.out.println(s);
+	  }
+	  catch(IOException e){
+	      System.out.println("Invalid");
+	  }
+	}
+	static int sumOfNaturalNos(int n){
+	    int sum = 0;
+	    for(int i = 1; i<=n; i++){
+	        sum = sum + i;
+	    }
+	    return sum;
+	   
+	    
+	}
+}
